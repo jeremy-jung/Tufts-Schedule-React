0bd8988367d07b1e8acddd1bb04a9ab887d8e551
Jeremy Jung <jangho.j@gmail.com>
2021-01-09 20:33:58 -0500

    ADD: Facebook Login

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -8,7 +8,10 @@ import Popup from 'reactjs-popup';
 import CoursesSelectedList from './views/CoursesSelectedList.js';
 import CourseNameRecommendation from './views/CourseNameRecommendation.js';
 import OptionsMainList from './views/OptionsMainList.js';
+import ButtonFBLogin from './views/ButtonFBLogin.js';
 import style from './views/styles/Homepage.module.css';
+
+/* scripts */
 
 class Homepage extends React.Component {
     constructor(props) {
@@ -25,7 +28,6 @@ class Homepage extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         
     }
-
 
     /*
      * handleCheckID
@@ -136,6 +138,7 @@ class Homepage extends React.Component {
     componentDidMount() {
         // initialize {listCourseIDs} in this component's state
         this.getListCourseIDs();
+
     }
     
     async getListCourseIDs() {
@@ -181,6 +184,7 @@ class Homepage extends React.Component {
             let pop;
                 return (
                     <div className={style.container}>
+                        
                         <CoursesSelectedList handleGenerate = {this.handleGenerate.bind(this)} selectedCourses={this.state.selectedCourses} listCourseIDs = {this.state.listCourseIDs}>
                             <input type = "submit"></input>
                         </CoursesSelectedList>
@@ -198,7 +202,8 @@ class Homepage extends React.Component {
                                 </form>
                             </div>
                         </div>
-                        <OptionsMainList></OptionsMainList>
+                        <ButtonFBLogin></ButtonFBLogin>
+
                     </div>
                 );
         }
---
+++ frontend/src/components/views/scripts/fb.js
@@ -0,0 +1,42 @@
+/* Written by Jeremy Jung (9/1/2021)
+    async get Facebook javascript SDK
+*/
+
+const facebookAppId = process.env.REACT_APP_FACEBOOK_APP_ID;
+
+export default function initFBSDK () {
+    return new Promise(resolve => {
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        window.fbAsyncInit = function () {
+            console.log(facebookAppId);
+            window.FB.init({
+                appId: facebookAppId,
+                cookie: true,
+                xfbml: true,
+                version: 'v9.0'
+            });
+            
+            // check if user is logged in to facebook
+            window.FB.getLoginStatus(({ authResponse }) => {
+
+                if (authResponse) {
+                    console.log("Already logged in to facebook");
+                } else {
+                    console.log("Not logged in to facebook");
+                    resolve();
+                }
+
+            });
+
+            window.FB.AppEvents.logPageView();
+        }
+
+    })
+}
