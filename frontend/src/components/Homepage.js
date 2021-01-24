59aba99ce652d333e1eb1918d2a78d40e3c46761
Duncan <19duncanc@tas.tw>
2021-01-25 00:56:51 +0800

    post requet

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -4,7 +4,6 @@
 */
 
 import React from 'react';
-// import Popup from 'reactjs-popup';
 import CoursesSelectedList from './views/CoursesSelectedList.js';
 import CourseNameRecommendation from './views/CourseNameRecommendation.js';
 import style from './views/styles/Homepage.module.css';
@@ -209,11 +208,7 @@ class Homepage extends React.Component {
     componentDidMount() {
         // initialize {listCourseIDs} in this component's state
         this.getListCourseIDs();
-        if (this.getObjectIDs().length != 0)
-        {
-            this.getRecSchedule();
-        }
-        // this.getRecSchedule();
+  
 
     }
     
@@ -234,96 +229,6 @@ class Homepage extends React.Component {
         });
     }
 
-    // maps through selected courses and push all object ids to array
-    getObjectIDs() {
-        let res = [];
-        this.state.selectedCourses.map(function(info) {
-            res.push(info._id);
-        })
-        return res;
-    }
-
-
-    // post requests recommended schedule made by API
-    async getRecSchedule() {
-        console.log("selected Courses: " , this.state.selectedCourses);
-        console.log("object ids: ", this.getObjectIDs());
-        var API_URL = process.env.REACT_APP_API_URL + "/courses/schedule";
-        var requestDetail = {
-            "objectIds": this.getObjectIDs(),
-            "filter": {
-              "time": {
-                "Monday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ],
-                "Tuesday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ],
-                "Wednesday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ],
-                "Thursday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ],
-                "Friday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ],
-                "Saturday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ],
-                "Sunday": [
-                  {
-                    "time_earliest": "00:00",
-                    "time_latest": "23:59"
-                  }
-                ]
-              }
-            }
-          };
-
-
-        fetch(API_URL, {
-            method: 'POST',
-            mode: 'cors', // no-cors, *cors, same-origin
-            headers: {
-            'Content-Type': 'application/json'
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(requestDetail) // body data type must match "Content-Type" header
-        })
-        .then(response => response.json())
-        .then(result => {
-            //if the request is valid
-            this.setState({
-                eventInfo: result.data
-            });
-        },
-            (error) => {
-                console.log("error", error);
-        });
-        
-        console.log("post request event: ", this.state.eventInfo);
-
-    }
 
 
     /* CourseID input scripts */
@@ -470,7 +375,7 @@ class Homepage extends React.Component {
                             </div>
                             <br/>
 
-                            {this.state.renderSchedule ? <Week courseSchedule={true} eventInfo={this.state.eventInfo}></Week> : <div></div>}
+                            {this.state.renderSchedule ? <Week courseSchedule={true} selectedCourses={this.state.selectedCourses} eventInfo={this.state.eventInfo}></Week> : <div></div>}
     
     
     
--- frontend/src/components/views/calendars/Week.js
+++ frontend/src/components/views/calendars/Week.js
@@ -10,14 +10,109 @@ class Week extends React.Component {
         super(props);
         this.state = {
             dows: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            selectedCourses: this.props.selectedCourses,
         }
 
         console.log("event Info: ", this.props.eventInfo);
     }
 
+    componentDidMount() {
+        this.getRecSchedule();
+    }
+
+    // maps through selected courses and push all object ids to array
+    getObjectIDs() {
+        let res = [];
+        this.state.selectedCourses.map(function(info) {
+            res.push(info._id);
+        })
+        return res;
+    }
 
 
+    // post requests recommended schedule made by API
+    async getRecSchedule() {
+        console.log("selected Courses: " , this.state.selectedCourses);
+        console.log("object ids: ", this.getObjectIDs());
+        var API_URL = process.env.REACT_APP_API_URL + "/courses/schedule";
+        var requestDetail = {
+            "objectIds": this.getObjectIDs(),
+            "filter": {
+            "time": {
+                "Monday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ],
+                "Tuesday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ],
+                "Wednesday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ],
+                "Thursday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ],
+                "Friday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ],
+                "Saturday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ],
+                "Sunday": [
+                {
+                    "time_earliest": "00:00",
+                    "time_latest": "23:59"
+                }
+                ]
+            }
+            }
+        };
 
+
+        fetch(API_URL, {
+            method: 'POST',
+            mode: 'cors', // no-cors, *cors, same-origin
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(requestDetail) // body data type must match "Content-Type" header
+        })
+        .then(response => 
+            console.log("response post: " , response))
+            // response.json())
+        .then(result => {
+            //if the request is valid
+            console.log("result post: " , result);
+            this.setState({
+                eventInfo: result.data
+            });
+        },
+            (error) => {
+                console.log("error", error);
+        });
+        
+        console.log("post request event: ", this.state.eventInfo);
+
+    }
 
 
 
@@ -27,6 +122,7 @@ class Week extends React.Component {
 
 
     render () {
+        console.log("props eventinfo: ", this.props.eventInfo);
         return(
             <div className={weekStyle.weekContainer}> 
                 <Day dow="" timeBar={true} courseSchedule={this.props.courseSchedule}></Day>
